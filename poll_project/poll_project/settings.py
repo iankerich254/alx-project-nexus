3564f1272161a389d7c353636ac11134ac66257c
Ian Kerich <iankerich254@gmail.com>
2025-08-09 16:38:24 +0300

    Comment out DEBUG in Django settings

--- poll_project/poll_project/settings.py
+++ poll_project/poll_project/settings.py
@@ -31,7 +31,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+#DEBUG = env('DEBUG')
 
 # ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'alx-project-nexus-online-poll-kerich.onrender.com']
